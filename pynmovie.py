1171c41910053aa1148192dcdd372273083c65c7
Ben Keller <malzraa@gmail.com>
2012-08-16 16:24:44 -0400

    Added the userscript feature to pynmovie.

--- pynmovie.py
+++ pynmovie.py
@@ -12,6 +12,8 @@ if __name__ == "__main__":
 	help="The range of values to plot (in the format 'min max')")
 	parser.add_option("-t", "--type", action="store", dest="ptype", 
 	help="Type of particles to show (gas, dm, or star)", default="gas")
+	parser.add_option("-e", "--type", action="store", dest="script", 
+	help="preprocessing script to run on each simulation.")
 	(opts, args) = parser.parse_args()
 	imgcount = 0
 	fname = "pynmovie_"+opts.ptype+".mp4"
@@ -19,10 +21,15 @@ if __name__ == "__main__":
 		(vmin, vmax) = [float(i) for i in opts.valrange.split()]
 	except AttributeError:
 		(vmin, vmax) = (None, None)
+	if opts.script != None:
+		scriptfile = __import__(opts.script)
 	for i in args:
 		sim = pynbody.load(i)
-		ptypes = {"gas":sim.gas, "dm":sim.dm, "stars":sim.stars}
-		p_sph.image(ptypes[opts.ptype], cmap="hot", units="Msol kpc^-2", vmin=vmin, vmax=vmax)
+		if opts.script != None:
+			scriptfile.process(sim, i)
+		else:
+			ptypes = {"gas":sim.gas, "dm":sim.dm, "stars":sim.stars}
+			p_sph.image(ptypes[opts.ptype], cmap="hot", units="Msol kpc^-2", vmin=vmin, vmax=vmax)
 		plt.savefig("%09d.png" % (imgcount))
 		imgcount += 1
 	vid = envoy.run('ffmpeg  -b 1800 -r 5 -i %09d.png '+fname)
