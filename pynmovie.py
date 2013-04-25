7a309c94bd05048a1d517d60794c8ef5f7a7914f
Ben Keller <malzraa@gmail.com>
2013-04-25 14:33:59 -0400

    Added the ability to specify the filename output by pynmovie, as well as not actually generate a video.

--- pynmovie.py
+++ pynmovie.py
@@ -16,11 +16,14 @@ if __name__ == "__main__":
 	help="Frame Rate", default=5)
 	parser.add_option("-t", "--type", action="store", dest="ptype", 
 	help="Type of particles to show (gas, dm, or star)", default="gas")
+	parser.add_option("-o", "--outname", action="store", dest="outname", 
+	help="Filename of the video to be produced", default="pynmovie.mp4")
 	parser.add_option("-e", action="store", dest="script", 
 	help="preprocessing script to run on each simulation.")
+	parser.add_option("-n", action="store_false", dest="video", 
+	help="Don't actually produce a video, just make pngs.", default=True)
 	(opts, args) = parser.parse_args()
 	imgcount = 0
-	fname = "pynmovie_"+opts.ptype+".mp4"
 	try:
 		(vmin, vmax) = [float(i) for i in opts.valrange.split()]
 	except AttributeError:
@@ -37,6 +40,7 @@ if __name__ == "__main__":
 			p_sph.image(ptypes[opts.ptype], units='m_p cm**-3', cmap="jet", vmin=vmin, vmax=vmax)
 		plt.savefig("%09d.png" % (imgcount), dpi=150)
 		imgcount += 1
-	vid = envoy.run('ffmpeg  -qscale 1 -r %d -i %%09d.png %s' % (int(opts.fps), fname))
-	for i in range(imgcount):
-		envoy.run("rm %09d.png" % (i))
+	if opts.video:
+		vid = envoy.run('ffmpeg  -qscale 1 -r %d -i %%09d.png %s' % (int(opts.fps), opts.outname))
+		for i in range(imgcount):
+			envoy.run("rm %09d.png" % (i))
